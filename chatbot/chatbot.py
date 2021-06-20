87441c5fc9f8ef41a6893369978e1aa365a9c974
Siya Garg <siya.12.garg@gmail.com>
2021-06-20 18:24:10 +0530

    chatbot

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -12,7 +12,19 @@ app=Flask(__name__)
 
 '''this is for storing our data
 "Chatterbot" is the username, storage adap is for creating the database'''
-bot=ChatBot("Heal Helper")
+bot=ChatBot("Jifyy",
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch',
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand. I am still learning.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database.sqlite3')
 #eng_bot=ChatBot("Chatterbot",storage_adapter="chatterbot.storage.SQLStorageAdapter")
 #train data
 trainer=ChatterBotCorpusTrainer(bot)
